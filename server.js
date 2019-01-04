25d701cdc8844c9a1fd3f7addcf971bd8c2d0322
Carlos_Mina <cmina@nexura.com>
2019-01-04 11:55:17 -0500

    cambios

--- server.js
+++ server.js
@@ -201,11 +201,12 @@ function consultarServicio(tipo, cedula) {
     //console.log("SERVER_>_>_>_>_>", JSON.stringify(servicioAfiliadoEPS.servicioAfiliadoEPS.servicioQuemado("CC", "1107063182")));
     //  console.log("SERVER_>_>_>_>_>", servicioAfiliadoEPS.servicioAfiliadoEPS.armaObjetos(tipo, cedula));
     //let data = JSON.parse(servicioAfiliadoEPS.servicioAfiliadoEPS.servicioQuemado("CC", "1107063182"));
-    //let datos = servicioAfiliadoEPS.servicioAfiliadoEPS.armaObjetos(tipo, cedula);
-    utilities.utilities.functionWithCallBack(servicioAfiliadoEPS.servicioAfiliadoEPS.armaObjetos(tipo, cedula), 1000).then(function (res) {
-        console.log('res-> ', res);
-    });
-    // console.log("BODY__>__>__>__>", datos);
+    var datos = servicioAfiliadoEPS.servicioAfiliadoEPS.armaObjetos(tipo, cedula);
+    /* utilities.utilities.functionWithCallBack(servicioAfiliadoEPS.servicioAfiliadoEPS.armaObjetos(tipo, cedula),1000).then(res=>{
+        console.log('res-> ',res);
+        
+    }) */
+    console.log("BODY__>__>__>__>", datos);
     //console.log("BODY__>__>__>__>", datos.responseMessageOut.body.response.consultaAfiliadoResponse.afiliado);
     /* if (datos.responseMessageOut.body != undefined) {
         console.log("Existe");
